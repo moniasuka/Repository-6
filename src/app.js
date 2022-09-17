6d8833b3546c8ea118e71afe7118c07005a814ee
moniasuka <moniasuka@hotmail.com>
2022-09-17 02:19:15 +0000

    push generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,48 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let quien1 = ["Mi", "La", "Mi", "Los"];
+  let quien2 = ["perro", "gata", "hermana", "vecinos"];
+  let hizo1 = ["se", "se", "se", "se"];
+  let hizo2 = ["vomito", "durmio", "desmayo", "fueron"];
+  let textoh1 = document.querySelector("#generador");
+
+  let placeholder = "";
+  placeholder =
+    "<p>" +
+    quien1[Math.floor(Math.random() * quien1.length)] +
+    " " +
+    quien2[Math.floor(Math.random() * quien2.length)] +
+    " " +
+    hizo1[Math.floor(Math.random() * hizo1.length)] +
+    " " +
+    hizo2[Math.floor(Math.random() * hizo2.length)] +
+    "</p>";
+  textoh1.innerHTML = placeholder;
+  console.log(textoh1);
+
+  /*
+  for (let i = 0; i < quien1.length; i++) {
+    for (let j = 0; j < quien2.length; j++) {
+      for (let k = 0; k < hizo1.length; k++) {
+        for (let l = 0; l < hizo2.length; l++) {
+          console.log(
+            quien1[i] + " " + quien2[j] + " " + hizo1[k] + " " + hizo2[l]
+          );
+          placeholder +=
+            "<p>" +
+            quien1[i] +
+            " " +
+            quien2[j] +
+            " " +
+            hizo1[k] +
+            " " +
+            hizo2[l] +
+            "</p>";
+        }
+      }
+    }
+  }
+  textoh1.innerHTML = placeholder;
+*/
 };
